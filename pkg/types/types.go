481783b84b872b2b349db707d1639f7accb8f43f
delgoden <delgoden@gmail.com>
2021-04-02 19:11:48 +0300

    Changed payment structure added status field

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -6,9 +6,20 @@ type Money int64
 // Category the category in which the payment was made (cars, pharmacies, food, etc.)
 type Category string
 
+// Status payment status
+type Status string
+
+// Predefined payment statuses
+const (
+	StatusOk         Status = "OK"
+	StatusFail       Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
 // Payment payment information
 type Payment struct {
 	ID       int
 	Amount   Money
 	Category Category
+	Status   Status
 }

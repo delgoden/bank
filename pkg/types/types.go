eaf7a6dd00ebe7c3486897ca3ee9da17878963cb
delgoden <delgoden@gmail.com>
2021-09-14 09:22:29 +0300

    added new types

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -6,6 +6,16 @@ type Money int64
 // Category represents the category in which the payment was made (auto, pharmacy, restaurants, etc.)
 type Category string
 
+// Status represents the status of the payment
+type Status string
+
+//
+const (
+	StatusOk Status = "OK"
+	StatusFail Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
 // Payment provides information about the payment
 type Payment struct {
 	ID       int
